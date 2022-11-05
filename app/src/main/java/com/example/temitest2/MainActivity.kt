ea8b071c795b3b946f3018cba49677970a07be6a
DerLurchi <77876977+DerLurchi@users.noreply.github.com>
2022-11-05 23:17:17 +0100

    Legende eingebaut

--- app/src/main/java/com/example/temitest2/MainActivity.kt
+++ app/src/main/java/com/example/temitest2/MainActivity.kt
@@ -13,12 +13,17 @@ import android.content.Context
 import androidx.compose.ui.platform.LocalContext
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.colorspace.Rgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Device
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.unit.dp
@@ -60,12 +65,8 @@ import com.robotemi.sdk.voice.ITtsService
 import com.robotemi.sdk.voice.model.TtsVoice
 
 class MainActivity : ComponentActivity() {
-    private lateinit var robot: Robot
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
 
         setContent {
             TemiTest2Theme {
@@ -81,12 +82,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// Function to generate a Toast
-private fun mToast(context: Context, robot:Robot?, ziel:String){
-//    var robot: Robot = getInstance()
+// Funktion um den Roboter an einen Ort zu schicken
+private fun roboterGeheZu(context: Context, robot:Robot?, ziel:String, beschreibung: String){
+    val basistext = "Ich bringe dich "
+    val ttsRequest = create(speech = basistext + beschreibung, true)
+
     Toast.makeText(context, "Ich bringe dich an den Ort: $ziel", Toast.LENGTH_LONG).show()
     if (robot != null) {
-        robot.goTo(location = ziel, backwards = true,)
+        robot.goTo(location = ziel, backwards = false)
+        robot.speak(ttsRequest)
+    }
+}
+
+// Funktion um den Hifletext nochmal anzusagen
+private fun hilfeDurchsage(context: Context, robot:Robot?, ansage: String){
+    Toast.makeText(context, ansage, Toast.LENGTH_LONG).show()
+
+    if (robot != null) {
+        val ttsRequest = create(speech = ansage, true)
+        robot.speak(ttsRequest)
     }
 }
 
@@ -94,80 +108,216 @@ private fun mToast(context: Context, robot:Robot?, ziel:String){
 fun Navigation(){
     var robot: Robot?
 
+//  Damit kann die robot variable auch "null" sein und
+//  der roboter wird nur instanziert wenn er vorhanden ist
     try {
         robot = getInstance()
     } catch (e: Exception) {
         robot = null
     }
 
+//  Noetig fuer die Toast Nachricht
+    val mContext = LocalContext.current
+//  Noetig um die App zu schliessen
+    val activity = (LocalContext.current as? Activity)
 
+    val ansage : String = "Hi, ich bin Temi. Ich kann dir helfen, " +
+            "einen Ort in diesem Gebäude zu finden. " +
+            "Wenn ich deinen Ort aufzähle, unterbreche mich mit “Hey Temi” " +
+            "und nenne mir deinen Ort. "
+
+
+//  Start des Screenlayouts
     Box(modifier = Modifier.fillMaxSize()) {
-
-        val mContext = LocalContext.current
-        val activity = (LocalContext.current as? Activity)
 
         val hintergrund = painterResource(id = R.drawable.karte_keller)
 
+//      Karte als Hintergrund
         Image(
             painter = hintergrund,
             contentDescription = null,
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { hilfeDurchsage(mContext, robot, ansage) },
             contentScale = ContentScale.Crop
         )
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        Button(onClick = {
-            activity?.finish()
-        },
-            Modifier
+//      Schliessen Button (versteckt)
+        Button(
+            onClick = { activity?.finish() },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF3C3C3C)),
+            modifier = Modifier
                 .align(Alignment.TopEnd)
-                .padding(top = 60.dp, end = 60.dp)
+                .size(100.dp)
             ) {
             Text(
-                text = "Exit",
+                text = "",
                 fontSize = 30.sp,
             )
         }
 
-        IconButton(
-            onClick = { mToast(mContext, robot, "sitzecke") },
+//      Hilfebutton
+        Button(
+            onClick = { hilfeDurchsage(mContext, robot, ansage) },
             modifier = Modifier
-                .height(100.dp)
+                .size(60.dp)
+                .offset(30.dp, 30.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF49EACC)),
+            contentPadding = PaddingValues(7.dp)
+            ) {
+            Image(painter = painterResource(id = R.drawable.fragezeichen),
+                contentDescription = "Hilfe")
+        }
+
+//      Hotspotbuttons
+        Goto_Btn(ort = "hinteres labor",
+            icon_drawable = R.drawable.hinteres_labor,
+            beschreibung = "zum hinteren Labor",
+            context = mContext,
+            robot = robot,
+            modifier = Modifier
                 .align(Alignment.Center)
-            ,) {
-            Image(
-                painter = painterResource(id = R.drawable.temi),
-                contentDescription = null )
-        }
-        IconButton(
-            onClick = { mToast(mContext, robot, "home base") },
+                .offset(120.dp, 100.dp)
+        )
+
+        Goto_Btn(ort = "aufzug",
+            icon_drawable = R.drawable.aufzug,
+            beschreibung = "zum Aufzug",
+            context = mContext,
+            robot = robot,
             modifier = Modifier
-                .height(100.dp)
-                .align(Alignment.CenterStart)
-                .padding(start = 60.dp)) {
-            Image(
-                painter = painterResource(id = R.drawable.temi),
-                contentDescription = null )
-        }
-        IconButton(
+                .align(Alignment.Center)
+                .offset((-170).dp, (-130).dp)
+        )
+
+        Goto_Btn(ort = "treppe",
+            icon_drawable = R.drawable.treppe,
+            beschreibung = "zur Treppe",
+            context = mContext,
+            robot = robot,
             modifier = Modifier
-                .height(100.dp)
-                .align(Alignment.CenterEnd)
-                .padding(end= 60.dp)
-            ,
-            onClick = { mToast(mContext, robot, "hinteres labor") }) {
-            Image(
-                painter = painterResource(id = R.drawable.temi),
-                contentDescription = null )
+                .align(Alignment.Center)
+                .offset((-270).dp, (-50).dp)
+        )
+
+        Goto_Btn(ort = "eingang",
+            icon_drawable = R.drawable.eingang,
+            beschreibung = "zum Eingang",
+            context = mContext,
+            robot = robot,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset(10.dp, 30.dp)
+        )
+
+        Goto_Btn(ort = "herren-wc",
+            icon_drawable = R.drawable.h_wc,
+            beschreibung = "zum Herren-WC",
+            context = mContext,
+            robot = robot,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset((-30).dp, (-50).dp)
+        )
+
+        Goto_Btn(ort = "damen-wc",
+            icon_drawable = R.drawable.d_wc,
+            beschreibung = "zum Damen-WC",
+            context = mContext,
+            robot = robot,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset(50.dp, (-50).dp)
+        )
+
+        Goto_Btn(ort = "behinderten-wc",
+            icon_drawable = R.drawable.be_wc,
+            beschreibung = "zum Behinderten-WC",
+            context = mContext,
+            robot = robot,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset(200.dp, (-30).dp)
+        )
+
+        Goto_Btn(ort = "sitzecke",
+            icon_drawable = R.drawable.sitzecke,
+            beschreibung = "zur sitzecke",
+            context = mContext,
+            robot = robot,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset((-20).dp, 120.dp)
+            )
+
+//      Legende
+        Column (modifier = Modifier
+            .align(Alignment.BottomEnd)
+            .padding(end = 30.dp, bottom = 50.dp),
+        ) {
+            Text(text = "Legende", color = Color.White, fontWeight = FontWeight(600))
+            Legendenpunkt(icon_drawable = R.drawable.be_wc, ziel = "Behinderten-WC")
+            Legendenpunkt(icon_drawable = R.drawable.d_wc, ziel = "Damen-WC")
+            Legendenpunkt(icon_drawable = R.drawable.h_wc, ziel = "Herren-WC")
+            Legendenpunkt(icon_drawable = R.drawable.aufzug, ziel = "Aufzug")
+            Legendenpunkt(icon_drawable = R.drawable.treppe, ziel = "Treppe")
+            Legendenpunkt(icon_drawable = R.drawable.eingang, ziel = "Eingang")
+            Legendenpunkt(icon_drawable = R.drawable.sitzecke, ziel = "Sitzecke")
+            Legendenpunkt(icon_drawable = R.drawable.hinteres_labor, ziel = "Hinteres Labor")
         }
+    }
+}
+
+@Composable
+fun Goto_Btn(ort:String,
+             icon_drawable:Int,
+             beschreibung:String,
+             context:Context,
+             robot:Robot?,
+             modifier : Modifier = Modifier){
+    IconButton(
+        onClick = { roboterGeheZu(context, robot, ort, beschreibung) },
+        modifier = modifier
+            .height(100.dp),
+        ) {
+        Box (contentAlignment = Alignment.Center){
+            Image(
+                painter = painterResource(id = R.drawable.drop),
+                contentDescription = null,
+                )
+            Image(
+                painter = painterResource(id = icon_drawable),
+                contentDescription = beschreibung,
+                modifier = Modifier
+                    .padding(23.dp)
+                    .offset(y = (-10).dp)
+
+                )
+        }
+    }
+}
+
+@Composable
+fun Legendenpunkt(icon_drawable: Int, ziel : String){
+    Row (modifier = Modifier.padding(top = 10.dp)){
+        Image(
+            painter = painterResource(id = icon_drawable),
+            contentDescription = null,
+            modifier = Modifier
+                .height(20.dp)
+                .padding(end = 10.dp),
+            colorFilter = ColorFilter.tint(color = Color(0xFF49EACC))
+        )
+        Text(text = ziel, color = Color.White)
     }
 }
 
 @Preview(
     showBackground = true,
     showSystemUi = true,
-    device = "spec:width=1920dp,height=1080dp,dpi=224"
+    device = "spec:width=1920px,height=1080px,dpi=224, orientation=landscape"
 )
 @Composable
 fun DefaultPreview() {
